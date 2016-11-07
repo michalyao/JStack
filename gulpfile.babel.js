911eabbad76c4e842ab969d4b405b63257949c69
michalyao <yaoyao0777@Gmail.com>
2016-11-07 21:10:48 +0800

    add webpack in gulp task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,27 +3,28 @@
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import del from 'del';
-import { exec } from 'child_process';
 import eslint from 'gulp-eslint';
+import webpack from 'webpack-stream';
+import webpackConfig from './webpack.config.babel';
 
 const paths = {
-  allSrcJs: 'src/**/*.js',
+  allSrcJs: 'src/**/*.js?(x)',
+  serverSrcJs: 'src/server/**/*.js?(x)',
+  sharedSrcJs: 'src/shared/**/*.js?(x)',
+  clientEntryPoint: 'src/client/app.js',
   libDir: 'lib',
   gulpFile: 'gulpfile.babel.js',
   webpackFile: 'webpack.config.babel.js',
   distDir: 'dist',
+  clientBundle: 'dist/client-bundle.js?(.map)',
 };
 
 gulp.task('clean', () => del(paths.libDir));
 
 gulp.task('build', ['clean', 'lint'], () => gulp.src(paths.allSrcJs).pipe(babel()).pipe(gulp.dest(paths.libDir)));
 
-gulp.task('main', ['build'], (callback) => {
-  exec(`node ${paths.libDir}`, (error, stdout) => {
-    console.log(stdout);
-    return callback(error);
-  });
-});
+gulp.task('main', ['lint', 'clean'], () => gulp.src(paths.clientEntryPoint)
+                                  .pipe(webpack(webpackConfig)).pipe(gulp.dest(paths.distDir)));
 
 gulp.task('watch', () => gulp.watch(paths.allSrcJs, ['main']));
 
